d8d782f280d826a6d4c31274f21d1afd478dbd71
Diego Fernández Sierra <dfernandezs@fpcoruna.afundacion.org>
2022-09-28 09:39:41 +0200

    Tarea 2 completada

--- sprint0python/concurso.py
+++ sprint0python/concurso.py
@@ -1,3 +1,4 @@
+punt = 0
 print ("Quien ganó el mundial de 2010?")
 print("a.Brasil")
 print("b.Holanda")
@@ -5,8 +6,36 @@ print("c.España")
 op=input("Teclea tu respuesta: ")
 while(op!="a")and(op!="b")and(op!="c"):
 	op=input("Teclea tu respuesta: ")
-	
 if(op=="a")or(op=="b"):
 	print("Respuesta incorrecta")
+	punt = punt -5
 elif(op=="c"):
 	print("RESPUESTA CORRECTA")
+	punt = punt +10
+print ("Cual es la capital de Francia?")
+print("a.Paris")
+print("b.Versalles")
+print("c.Eiffel")
+op=input("Teclea tu respuesta: ")
+while(op!="a")and(op!="b")and(op!="c"):
+	op=input("Teclea tu respuesta: ")
+if(op=="c")or(op=="b"):
+	print("Respuesta incorrecta")
+	punt = punt -5
+elif(op=="a"):
+	print("RESPUESTA CORRECTA")
+	punt = punt + 10
+print ("Quien es el presidente de España?")
+print("a.Pedro Sánchez")
+print("b.Mariano Rajoy")
+print("c.José María Aznar")
+op=input("Teclea tu respuesta: ")
+while(op!="a")and(op!="b")and(op!="c"):
+	op=input("Teclea tu respuesta: ")
+if(op=="c")or(op=="b"):
+	print("Respuesta incorrecta")
+	punt = punt-5
+elif(op=="a"):
+	print("RESPUESTA CORRECTA")
+	punt = punt+10
+print(punt)
